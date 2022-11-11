9464776e6139cef2ba0c66abb387f6dcb617e6f9
my8za <emilie15@naver.com>
2022-11-11 13:14:28 +0900

    complete react-router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // style
 import '../styles/body.scss';
 // components
@@ -10,6 +10,7 @@ import Doctor from './Doctor';
 import Outpatient from './Outpatient';
 import WardManagement from './WardManagement';
 import WardManagement2 from './WardManagement2';
+import MyPage from './MyPage';
 
 const index = () => {
   return(
@@ -20,8 +21,12 @@ const index = () => {
           <nav><Nav /></nav>
           <div>
             <Routes>
-              <Route path="/" element={<Reception />} />
-              <Route path="/" element={<></>} />
+              <Route path="/reception" element={<Reception />} />
+              <Route path="/doctor" element={<Doctor/>} />
+              <Route path='/outpatient' element={<Outpatient/>} />
+              <Route path='/ward-management' element={<WardManagement/>} />
+              <Route path='/ward-management2' element={<WardManagement2/>} />
+              <Route path='/my-page' element={<MyPage/>} />
               <Route path='/' element={<></>} />
               <Route path='/' element={<></>} />
             </Routes>
